90080109e40ef64fabc69d03f48d879a19b5284f
Jingmin Yang <974394481@qq.com>
2017-07-05 13:12:47 +0800

    0705update

--- BombCatcher/BombCatcher/MyGame.cpp
+++ BombCatcher/BombCatcher/MyGame.cpp
@@ -70,4 +70,84 @@ void Game_Run(HWND window) {
 		bucket.y += 6.0f;
 
 	//move the bucket with the controller
+	if (XInput_Controller_Found()) {
+		//left analog thumb stick
+		if (controllers[0].sThumbLX < -5000)
+			bucket.x -= 6.0f;
+		else if (controllers[0].sThumbLX > 5000)
+			bucket.x += 6.0f;
+		//left and right triggers
+		if (controllers[0].bLeftTrigger > 128)
+			bucket.x -= 6.0f;
+		else if (controllers[0].bRightTrigger > 128)
+			bucket.x += 6.0f;
+		//left and right shoulers
+		if (controllers[0].wButtons & XINPUT_GAMEPAD_LEFT_SHOULDER)
+			bucket.x -= 6.0f;
+		else if (controllers[0].wButtons&XINPUT_GAMEPAD_RIGHT_SHOULDER)
+			bucket.x += 6.0f;
+		//left and right D-PAD
+		if (controllers[0].wButtons & XINPUT_GAMEPAD_DPAD_LEFT)
+			bucket.x -= 6.0f;
+		else if (controllers[0].wButtons&XINPUT_GAMEPAD_DPAD_RIGHT)
+			bucket.x += 6.0f;
+	}
+
+	//update vibration
+	if (vibrating > 0) {
+		vibrating++;
+		if (vibrating > 20) {
+			XInput_Vibrate(0, 0);
+			vibrating = 0;
+		}
+	}
+
+	//keep bucket inside the screen
+	if (bucket.x < 0) bucket.x = 0;
+	if (bucket.x > SCREENW - 128) bucket.x = SCREENW - 128;
+	//see if bucket caught the bomb
+	int cx = bomb.x + 64;
+	int cy = bomb.y + 64;
+	if (cx > bucket.x&&cx<bucket.x + 128 && cy>bucket.y&&cy < bucket.y + 128) {
+		//update and display score
+		score++;
+		std::ostringstream os;
+		os << APPTITLE << " [SCORE" << score << "]";
+		string scoreStr = os.str();
+		SetWindowText(window, scoreStr.c_str());
+		//vibrate the controller
+		XInput_Vibrate(0, 65000);
+		vibrating = 1;
+		//restart bomb
+		bomb.reset();
+	}
+
+	//clear the back buffer
+	d3ddev->ColorFill(backbuffer, NULL, D3DCOLOR_XRGB(0, 0, 0));
+	//start rendering
+	if (d3ddev->BeginScene()) {
+		//draw the bomb
+		DrawSurface(backbuffer, bomb.x, bomb.y, bomb_surf);
+
+		//draw the bucket
+		DrawSurface(backbuffer, bucket.x, bucket.y, bucket_surf);
+
+		//stop rendering
+		d3ddev->EndScene();
+		d3ddev->Present(NULL, NULL, NULL, NULL);
+	}
+
+	//escape key exits
+	if (Key_Down(DIK_SPACE) || Key_Down(DIK_ESCAPE))
+		gameover = true;
+	//controller back button also exits
+	if (controllers[0].wButtons & XINPUT_GAMEPAD_BACK)
+		gameover = true;
+}
+
+void Game_End() {
+	if (bomb_surf) bomb_surf->Release();
+	if (bucket_surf)bucket_surf->Release();
+	DirectInput_Shutdown();
+	Direct3D_Shutdown();
 }
